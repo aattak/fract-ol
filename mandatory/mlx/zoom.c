7adb27d29c8cf28ada78db7159ed259565fc5c9f
aattak <aattak@student.1337.ma>
2024-05-27 07:47:29 +0100

    update zoom

--- mandatory/mlx/zoom.c
+++ mandatory/mlx/zoom.c
@@ -6,7 +6,7 @@
 /*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 13:11:45 by aattak            #+#    #+#             */
-/*   Updated: 2024/05/26 19:42:25 by aattak           ###   ########.fr       */
+/*   Updated: 2024/05/27 07:10:44 by aattak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,28 @@
 
 void	zoom_in(t_data *data, int x, int y)
 {
-	(void)x;
-	(void)y;
-	ft_putstr("zoom in\n");
-//	double	old_scale;
-//
-//	old_scale = data->img.scale;
-//	data->img.scale /= ZOOM_IN;
-//	data->img.x_start = data->img.x_start + (x * (old_scale - data->img.scale));
-//	data->img.y_start = data->img.y_start + (y * (data->img.scale - old_scale));
-	data->img.x_norm *= ZOOM;
-	data->img.y_norm *= ZOOM;
+	double	old_x_norm;
+	double	old_y_norm;
+
+	old_x_norm = data->img.x_norm;
+	old_y_norm = data->img.y_norm;
+	data->img.x_norm /= ZOOM;
+	data->img.y_norm /= ZOOM;
+	data->img.x_move += (x * (old_x_norm - data->img.x_norm)) / (WIDTH - 1);
+	data->img.y_move += (y * (data->img.y_norm - old_y_norm)) / (HEIGHT - 1);
 	render_fractal(data);
 }
 
 void	zoom_out(t_data *data, int x, int y)
 {
-	(void)x;
-	(void)y;
-	ft_putstr("zoom out\n");
-//	double	old_scale;
-//
-//	old_scale = data->img.scale;
-//	data->img.scale *= ZOOM_OUT;
-//	data->img.x_start = data->img.x_start + (x * (old_scale - data->img.scale));
-//	data->img.y_start = data->img.y_start + (y * (data->img.scale - old_scale));
-	data->img.x_norm /= ZOOM;
-	data->img.y_norm /= ZOOM;
+	double	old_x_norm;
+	double	old_y_norm;
+
+	old_x_norm = data->img.x_norm;
+	old_y_norm = data->img.y_norm;
+	data->img.x_norm *= ZOOM;
+	data->img.y_norm *= ZOOM;
+	data->img.x_move += (x * (old_x_norm - data->img.x_norm)) / (WIDTH - 1);
+	data->img.y_move += (y * (data->img.y_norm - old_y_norm)) / (HEIGHT - 1);
 	render_fractal(data);
 }
